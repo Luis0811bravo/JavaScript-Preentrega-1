b56585a38b25b564fb269c25d74962ac24b757cb
Luis0811bravo <121514384+Luis0811bravo@users.noreply.github.com>
2024-09-02 22:00:48 -0600

    Se corrigio lógica de asignación

--- js/main.js
+++ js/main.js
@@ -10,37 +10,54 @@ usuario = usuario.toLowerCase();
 
 let nuevoProyecto = confirm('¿Quieres agregar un nuevo proyecto?');
 
-if (nuevoProyecto) {
-    let nombreProyecto = prompt('Selecciona área del experimento (insecticida, fungicida, herbicida, nematicida, acaricida, bactericida');
+if (nuevoProyecto == false) {
+    switch (usuario) {
+        case usuarioHaidiel:
+            // asignar();
+            console.log("INFORMES DEL DR " + usuario.toLocaleUpperCase());
+            
+            break;
+    
+        case usuarioAusencio:
+            console.log("INFORMES DEL DR " + usuario.toLocaleUpperCase());
+            break;
+    
+        case usuarioPaul:
+            console.log("INFORMES DEL DR " + usuario.toLocaleUpperCase());
+            break;
+    
+        case usuarioYuridia:
+            console.log("INFORMES DE LA DRA " + usuario.toLocaleUpperCase());
+            break;
+        
+        default:
+            alert('FAVOR DE REVISAR INFORMACIÓN DE ACCESO');
+    }
+
+} else {
+    
+    let nombreProyecto = prompt('Selecciona área del experimento (insecticida, fungicida, herbicida, nematicida');
     if (nombreProyecto == 'insecticida') {
-        alert('Proyecto asignado a ' + usuarioAusencio.toLocaleUpperCase());
+        alert('PROYECTO ASIGNADO AL DR' + usuarioHaidiel.toLocaleUpperCase());
+    } else if (nombreProyecto == 'herbicida') {
+        alert('PROYECTO ASIGNADO A LA DRA' + usuarioYuridia.toLocaleUpperCase());
+    } else if (nombreProyecto == 'fungicida') {
+        alert('PROYECTO ASIGNADO AL DR' + usuarioPaul.toLocaleUpperCase());
+    } else if (nombreProyecto == 'nematicida') {
+        alert('PROYECTO ASIGNADO AL DR' + usuarioAusencio.toLocaleUpperCase());
     }
 }
 
-//function asignar() {
-    
+//Hace falta una funcion para asignar la entrada por prompt 
 
-switch (usuario) {
-    case usuarioHaidiel:
-        // asignar();
-        console.log("INFORMES DE " + usuario.toLocaleUpperCase());
-        
-        break;
+//function asignar() { }
 
-    case usuarioAusencio:
-        console.log("INFORMES DE " + usuario.toLocaleUpperCase());
-        break;
+//Hace falta realizar un arreglo de todos los experimentos para así poder extraer los asignados a cada usuario 
 
-    case usuarioPaul:
-        console.log("INFORMES DE " + usuario.toLocaleUpperCase());
-        break;
+//Hace falta una funcion para poder seleccionar y mostrar los experimentos de cada usuario,  
 
-    case usuarioYuridia:
-        console.log("INFORMES DE " + usuario.toLocaleUpperCase());
-        break;
-    
-    default:
-        alert('FAVOR DE REVISAR INFORMACIÓN DE ACCESO');
-}
+//function extraer() { }    
+
+
 
 
